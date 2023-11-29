62758190408b326292267e6f2ed3d1ba7fea7e8d
Borovaneca <petyoveselinov@gmail.com>
2023-11-29 20:23:38 +0200

    Adding real-time chat feature which allows registered users to communicate in real time.

---
+++ src/test/java/com/example/carwash/web/controller/AppointmentControllerIT.java
@@ -0,0 +1,73 @@
+package com.example.carwash.web.controller;
+
+import com.example.carwash.model.entity.ProfileImage;
+import com.example.carwash.model.entity.User;
+import com.example.carwash.repository.ProfileImageRepository;
+import com.example.carwash.repository.RoleRepository;
+import com.example.carwash.repository.UserRepository;
+import com.example.carwash.service.interfaces.ViewService;
+import com.icegreen.greenmail.util.GreenMail;
+import com.icegreen.greenmail.util.ServerSetup;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@ExtendWith(MockitoExtension.class)
+public class AppointmentControllerIT {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private RoleRepository roleRepository;
+
+    @Autowired
+    private ViewService viewService;
+
+    @Autowired
+    private ProfileImageRepository profileImageRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    private User user;
+
+    @BeforeEach
+    void setUp() {
+        if (userRepository.count() == 0) {
+            user = registerUser();
+            userRepository.save(user);
+        }
+    }
+
+
+
+    private User registerUser() {
+        User admin = new User();
+        admin.setUsername("Admin");
+        admin.setPassword(passwordEncoder.encode("Adminov1"));
+        admin.setRoles(roleRepository.findAll());
+        admin.setImage(new ProfileImage("test"));
+        admin.setCity("Vratsa");
+        admin.setActive(true);
+        admin.setAge(23);
+        admin.setImage(profileImageRepository.save(new ProfileImage("test")));
+        admin.setEmail("borovaneca@softuni.bg");
+        admin.setFirstName("Petyo");
+        admin.setLastName("Veselinov");
+        admin.setBanned(false);
+        admin.setRegisteredOn(LocalDate.now());
+        admin.setSocialMedias(new ArrayList<>());
+        admin.setBio("Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis ducimus ad atque nulla. Reiciendis praesentium beatae quod cumque odit accusamus. Doloribus inventore voluptatem suscipit pariatur omnis aliquid non illo mollitia!");
+        return admin;
+    }
+}
