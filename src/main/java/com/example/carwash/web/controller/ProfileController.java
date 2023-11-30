1195e972660b390678e739110e092d130f610db6
Borovaneca <petyoveselinov@gmail.com>
2023-11-30 14:33:52 +0200

    Probably last commmit.. who knows?

--- src/main/java/com/example/carwash/web/controller/ProfileController.java
+++ src/main/java/com/example/carwash/web/controller/ProfileController.java
@@ -32,7 +32,6 @@ public class ProfileController {
     private final ViewService viewService;
     private final UserService userService;
 
-    private ProfileEditDTO profileEditDTO;
 
     @Autowired
     public ProfileController(ViewService viewService, @Qualifier("userServiceProxy") UserService userService) {
@@ -42,7 +41,7 @@ public class ProfileController {
 
     @ModelAttribute("profileEditDTO")
     public ProfileEditDTO profileEditDTO() {
-        return profileEditDTO;
+        return new ProfileEditDTO();
     }
 
 
@@ -76,8 +75,8 @@ public class ProfileController {
         checkIfAuthorized(userDetails, username);
 
         if (isValidUser(username)) {
-            if (profileEditDTO == null) {
-                profileEditDTO = userService.getUserAndMapToProfileEditDTO(username);
+            if (model.asMap().size() == 1) {
+                ProfileEditDTO profileEditDTO = userService.getUserAndMapToProfileEditDTO(username);
                 model.addAttribute("profileEditDTO", profileEditDTO);
             }
         }
