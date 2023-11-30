772d68376164d416fdaa989de09614fd48784568
Borovaneca <petyoveselinov@gmail.com>
2023-11-30 16:26:03 +0200

    Probably last commmit.. who knows?

--- src/main/java/com/example/carwash/web/controller/ContactController.java
+++ src/main/java/com/example/carwash/web/controller/ContactController.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -21,10 +22,12 @@ public class ContactController {
         this.emailService = emailService;
     }
 
+    @ModelAttribute("contact")
+    public ContactDTO contactDTO() {
+        return new ContactDTO();
+    }
     @GetMapping("/contact")
-    public String contact(ContactDTO contactDTO, Model model) {
-
-            model.addAttribute("contact", contactDTO);
+    public String contact() {
         return "contact";
     }
 
